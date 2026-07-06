8e640dcd184bc11cc06be0eb8e0549480cf09fc0
nyamyjese <hei.jese.2@gmail.com>
2026-07-06 13:10:11 +0100

    fix : add missing annotation to repository classes

--- src/main/java/hei/exo/sub/repository/CourseRepository.java
+++ src/main/java/hei/exo/sub/repository/CourseRepository.java
@@ -2,7 +2,8 @@ package hei.exo.sub.repository;
 
 import hei.exo.sub.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface CourseRepository extends JpaRepository<Course, UUID> {}
--- src/main/java/hei/exo/sub/repository/UserRepository.java
+++ src/main/java/hei/exo/sub/repository/UserRepository.java
@@ -2,7 +2,8 @@ package hei.exo.sub.repository;
 
 import hei.exo.sub.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {}
