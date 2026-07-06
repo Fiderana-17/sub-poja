19f26cb411b61327e43712f095cbdcdca3b95399
Gaël Raj <gaelrajaomiandrisoa@gmail.com>
2026-07-06 09:46:47 +0300

    feat: add global API exception handling

---
+++ src/main/java/hei/exo/sub/exception/model/ExceptionBody.java
@@ -0,0 +1,10 @@
+package hei.exo.sub.exception.model;
+
+import java.time.Instant;
+
+public record ExceptionBody(
+        int status,
+        String error,
+        String message,
+        String path,
+        Instant timestamp) {}
