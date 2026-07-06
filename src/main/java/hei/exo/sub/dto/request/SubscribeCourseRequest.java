3f3e342d4e859d1fe33d5b3fe79ec364473d1617
Gaël Raj <gaelrajaomiandrisoa@gmail.com>
2026-07-06 10:18:51 +0300

    feat: add course subscription DTOs

---
+++ src/main/java/hei/exo/sub/dto/request/SubscribeCourseRequest.java
@@ -0,0 +1,7 @@
+package hei.exo.sub.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import java.util.UUID;
+
+public record SubscribeCourseRequest(
+        @NotNull(message = "The user id is mandatory") UUID userId) {}
---
+++ src/main/java/hei/exo/sub/dto/response/UserResponse.java
@@ -0,0 +1,9 @@
+package hei.exo.sub.dto.response;
+
+import java.util.UUID;
+
+public record UserResponse(
+        UUID id,
+        String firstName,
+        String lastName,
+        String userName) {}
