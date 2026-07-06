9cafbf202f7f8db23dc6146edb221c4cea7a40d2
nyamyjese <hei.jese.2@gmail.com>
2026-07-06 14:07:14 +0100

    fix: add missing annotation in entity

--- src/main/java/hei/exo/sub/entity/CourseEntity.java
+++ src/main/java/hei/exo/sub/entity/CourseEntity.java
@@ -3,12 +3,15 @@ package hei.exo.sub.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Entity
 @Table(name = "course")
+@Data
+
 public class CourseEntity {
     @Id private UUID id;
     private String title;
--- src/main/java/hei/exo/sub/entity/SubscriptionEntity.java
+++ src/main/java/hei/exo/sub/entity/SubscriptionEntity.java
@@ -1,11 +1,14 @@
 package hei.exo.sub.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "subscription")
+@Data
+
 public class SubscriptionEntity {
     @Id private UUID id;
     private UUID userId;
